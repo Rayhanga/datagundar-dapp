f04381b6309aaec575ad652a2f8ef473a0c74fe5
Rayhan Gusty Alif <rayhangustyalif@gmail.com>
2022-01-24 22:02:17 +0700

    Add detected bug note

--- src/routes/dapp/index.svelte
+++ src/routes/dapp/index.svelte
@@ -1,3 +1,8 @@
+<script lang="ts">
+  // TODO: Fix Stateless Store when first initialization
+  // To replicate bug: logout, force refresh, login, there will be no states on any stores
+</script>
+
 <svelte:head>
   <title>DataGundar Dapp | Dashboard</title>
 </svelte:head>
