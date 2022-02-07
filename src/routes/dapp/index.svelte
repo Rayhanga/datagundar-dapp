2c1e30a7603221c42a255b383ed5fe7ad9c0d13b
Rayhan Gusty Alif <rayhangustyalif@gmail.com>
2022-02-07 17:20:05 +0700

    Add defaultcorsproxy to registeredcorsproxy
    This enables scraping without even needing to setup cors proxy

--- src/routes/dapp/index.svelte
+++ src/routes/dapp/index.svelte
@@ -1,6 +1,24 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+  import mainStore from "$lib/stores";
+
   // TODO: Fix Stateless Store when first initialization
   // To replicate bug: logout, force refresh, login, there will be no states on any stores
+
+  const { username, selectedCorsProxy, registeredCorsProxies } = mainStore;
+
+  onMount(() => {
+    if (username) {
+      if (!$selectedCorsProxy) {
+        const defaultCorsProxy = window.location.href.replace("/dapp", "/api/defaultcorsproxy")
+        registeredCorsProxies.addItem(
+          defaultCorsProxy,
+          defaultCorsProxy
+        );
+        selectedCorsProxy.setValue(defaultCorsProxy)
+      }
+    }
+  });
 </script>
 
 <svelte:head>
