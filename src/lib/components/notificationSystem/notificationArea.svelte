1634a76e2552504068d5a087dee34473259bf417
Rayhan Gusty Alif <rayhangustyalif@gmail.com>
2022-02-09 15:23:42 +0700

    Fix notification system

---
+++ src/lib/components/notificationSystem/notificationArea.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import mainStore from "$lib/stores";
+  import Notification from "./notification.svelte";
+
+  const { notifications } = mainStore;
+
+  const lifespan = 2000; // in ms
+</script>
+
+{#if $notifications}
+  {#if Object.entries($notifications).length !== 0}
+    <div class="fixed w-screen z-50 p-5">
+      {#each Object.entries($notifications) as [key, notification] (key)}
+        <Notification {key} {notification} {lifespan} />
+      {/each}
+    </div>
+  {/if}
+{/if}
